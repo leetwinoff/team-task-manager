d52fde70efd2746669a0eb6ee5d1e77f19a75326
leetwinoff <leetwinoff92@gmail.com>
2023-03-14 21:45:49 -0500

    Updated index view

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
-def index(request):
+from tasks.models import Worker, Task
 
-    return render(request, "tasks/index.html")
+
+def index(request):
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+
+    context = {
+        "num_workers": num_workers,
+        "num_tasks": num_tasks
+    }
+
+    return render(request, "tasks/index.html", context=context)
+
