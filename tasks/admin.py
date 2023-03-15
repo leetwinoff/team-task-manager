bb563e688813e260c4fdce3068f8debcc0f67f2e
leetwinoff <leetwinoff92@gmail.com>
2023-03-14 20:49:18 -0500

    Added registration on admin site

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {
+            "fields": ("years_of_experience", "phone_number"),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Additional info", {"fields": ("years_of_experience", "phone_number")}),
+    )
