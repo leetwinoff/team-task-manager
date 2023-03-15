4efeeedb09707cca657bbada400a7c9a0611a9d8
leetwinoff <leetwinoff92@gmail.com>
2023-03-14 20:27:35 -0500

    Added AUTH_USER_MODEL to settings

--- team_task_manager/settings.py
+++ team_task_manager/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'tasks'
 ]
 
+AUTH_USER_MODEL = "tasks.Worker"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
