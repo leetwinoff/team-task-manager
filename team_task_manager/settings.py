982934d085e82328317a4bee93f1367d9157bdf6
leetwinoff <leetwinoff92@gmail.com>
2023-04-23 12:57:14 -0500

    git add

--- team_task_manager/settings.py
+++ team_task_manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "@6-1k7*w_pyuiia1c98donr%s7-=3r73ehlioj0614v$1-f=^#")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
